2e4d13cb888c6c7e698e98083a91e2b0faddb313
SYDNEY FRANCIS <110589231+SydneyFrancis@users.noreply.github.com>
2022-12-04 00:36:50 +0530

    try

--- src/main/java/com/driver/models/Author.java
+++ src/main/java/com/driver/models/Author.java
@@ -10,6 +10,7 @@ import javax.persistence.*;
 import java.util.List;
 
 @Entity
+@AllArgsConstructor
 @Data
 public class Author {
 
--- src/main/java/com/driver/models/Student.java
+++ src/main/java/com/driver/models/Student.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
